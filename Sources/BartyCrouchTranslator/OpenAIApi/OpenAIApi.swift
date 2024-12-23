37fee94473f166693d2ffa6743edec2991d182bf
Christian Elbe <christian@hero21.app>
2024-12-23 15:35:35 +0100

    print

--- Sources/BartyCrouchTranslator/OpenAIApi/OpenAIApi.swift
+++ Sources/BartyCrouchTranslator/OpenAIApi/OpenAIApi.swift
@@ -158,7 +158,7 @@ extension OpenAIApi: Endpoint {
       )
       
       let messages = [systemMessage, userMessage]
-      
+      print("Starting API request with '\(messages)' ...")
       // Create a structured request object to encode as JSON.
       let requestPayload = OpenAIChatCompletionRequest(
         model: "gpt-4o",
